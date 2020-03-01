6062725de48573bffa4792d10089617c120c9360
Lvzbel <rcoto@protonmail.com>
2020-02-29 18:08:36 -0800

    Basic grid created

--- Fuego/src/App.js
+++ Fuego/src/App.js
@@ -6,6 +6,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col">Today's Top earners</div>
+          <div className="col">All Time Top earners</div>
+        </div>
+      </div>
     </div>
   );
 }
--- Fuego/src/components/Header.js
+++ Fuego/src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
   return (
     <nav class="navbar navbar-light bg-light">
-      <span class="navbar-brand mb-0 h1">Fuego</span>
+      <div className="container">
+        <span class="navbar-brand mb-0 h1">Fuego</span>
+      </div>
     </nav>
   );
 }
